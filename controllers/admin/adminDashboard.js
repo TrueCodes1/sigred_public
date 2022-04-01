851e4ae4de49fcee242d672706e38d8ddd83111f
norbertSviatko <informatikasviatko@gmail.com>
2022-04-01 19:54:49 +0200

    updated

--- controllers/admin/adminDashboard.js
+++ controllers/admin/adminDashboard.js
@@ -226,103 +226,73 @@ const getUser = async (req, res) => {
                     // IF THE COOKIE STRING EXISTS IN THE LIST OF COOKIE STRINGS OF ADMIN SESSIONS, TAKE FURTHER
                     // ACTIONS AND FINALLY SERVE THE ADMIN WITH THE ADMIN DASHBOARD SUBPAGE WITH ALL DATA FROM DB
                     if (adminsLoggedIn.includes(adminLoggedInCookie)) {
-                        let users = [];
-                        db.ref('/admin/users/').get()
+                        
+                        let userID = req.params.id;
+                        
+                        let userInfo = {
+                            info: {
+                                name: '',
+                                lastName: '',
+                                email: '',
+                                city: '',
+                                country: '',
+                                age: '',
+                                userName: ''
+                            },
+                            status: '',
+                            date: '',
+                            established: '',
+                            selling: {},
+                            bought: {},
+                            sold: {}
+                        }
+        
+                        db.ref('/users/'+userID).get()
                         .then((data) => {
                             let val = data.val();
-                            for (let user in val){
-                                // DEFINING VARIABLE AS OBJECT TO BE FILLED IN LATER ON WITH PERSONAL INFO OF THE USER
-                                let personal = {
-                                    name: '',
-                                    lastName: '',
-                                    userName: '',
-                                    age: '',
-                                    country: '',
-                                    city: '',
-                                    email: decrypt(val[user].email.encrypted, adminPWD.repeat(5).substring(0, 32), val[user].email.iv)
-                                    // DECRPYTING THE EMAIL ADDRESS OF THE USER
-                                }
-                                // DEFINING EMPTY LISTS FOR BOUGHT, SOLD AND SELLING ITEMS OF THE USER
-                                // DEFINING AN EMPTY STRING AS STATUS OF THE USER (ENABLED/DISABLED)
-                                let bought = [];
-                                let sold = [];
-                                let selling = [];
-                                let status = '';
-                                // DECRYPTING THE STATUS OF THE USER
-                                status = decrypt(val[user].status.encrypted, adminPWD.repeat(5).substring(0, 32), val[user].status.iv);
-                                // RECIEVING DATA ABOUT BOUGHT ITEMS OF EVERY USER
-                                if (val[user].bought.encrypted != undefined && val[user].bought.encrypted != null) {
-                                    for (let b of val[user].bought) {
-                                        let new_bought = {
-                                            buyer: decrypt(b.buyer.encrypted, adminPWD.repeat(5).substring(0, 32), b.buyer.iv),
-                                            seller: decrypt(b.seller.encrypted, adminPWD.repeat(5).substring(0, 32), b.seller.iv),
-                                            price: decrypt(b.price.encrypted, adminPWD.repeat(5).substring(0, 32), b.price.iv),
-                                            item: decrypt(b.item.encrypted, adminPWD.repeat(5).substring(0, 32), b.item.iv),
-                                            date: decrypt(b.date.encrypted, adminPWD.repeat(5).substring(0, 32), b.date.iv),
-                                            state: decrypt(b.state.encrypted, adminPWD.repeat(5).substring(0, 32), b.state.iv),
-                                        };
-                                        bought.push(new_bought)
-                                    }
-                                }
-                                // RECIEVING DATA ABOUT SOLD ITEMS OF EVERY USER
-                                if (val[user].sold.encrypted != undefined && val[user].sold.encrypted != null) {
-                                    for (let s of val[user].sold) {
-                                        let new_sold = {
-                                            buyer: decrypt(s.buyer.encrypted, adminPWD.repeat(5).substring(0, 32), s.buyer.iv),
-                                            seller: decrypt(s.seller.encrypted, adminPWD.repeat(5).substring(0, 32), s.seller.iv),
-                                            price: decrypt(s.price.encrypted, adminPWD.repeat(5).substring(0, 32), s.price.iv),
-                                            item: decrypt(s.item.encrypted, adminPWD.repeat(5).substring(0, 32), s.item.iv),
-                                            date: decrypt(s.date.encrypted, adminPWD.repeat(5).substring(0, 32), s.date.iv),
-                                            state: decrypt(s.state.encrypted, adminPWD.repeat(5).substring(0, 32), s.state.iv),
-                                        };
-                                        sold.push(new_sold)
-                                    }
-                                }
-                                db.ref('/users/'+user+'/personal-info').get()
-                                .then((data) => {
-                                    // 'FILLING' THE SO FAR (ALMOST) EMPTY OBJECT OF PERSONAL INFO OF THE USER
-                                    let vall = data.val();
-                                    personal.name = vall.name;
-                                    personal.lastName = vall.lastName;
-                                    personal.userName = vall.userName;
-                                    personal.age = vall.age;
-                                    personal.country = vall.country;
-                                    personal.city = vall.city;
-                                })
-                                .then(
-                                    db.ref('/users/'+user+'/selling').get()
-                                    .then((data) => {
-                                        // RETRIEVING DATA ABOUT ITEMS THAT ARE BEING OFFERED BY THE USER
-                                        let vall = data.val();
-                                        if (vall != '' && vall != ' ') {
-                                            for (let sl of vall.selling) {
-                                                let new_selling = {
-                                                    buyer: sl.buyer,
-                                                    seller: sl.seller,
-                                                    price: sl.price,
-                                                    item: sl.item,
-                                                    date: sl.date,
-                                                    state: sl.state
-                                                };
-                                                selling.push(new_selling)
-                                            }
-                                        }
-                                    })
-                                    .then(
-                                        // PUSHING THE CREATED OBJECT OF A USER TO THE LIST OF USERS
-                                        users.push({
-                                            status: status,
-                                            personal: personal,
-                                            bought: bought,
-                                            sold: sold,
-                                            selling: selling
-                                        })
-                                    )
-                                )
+                            //retrieving all information about user except for email (encrypted)
+                            userInfo.info.name = val['personal-info'].name;
+                            userInfo.info.lastName = val['personal-info'].lastName;
+                            userInfo.info.age = val['personal-info'].age;
+                            userInfo.info.city = val['personal-info'].city;
+                            userInfo.info.country = val['personal-info'].country;
+                            userInfo.info.userName = val['personal-info'].username;
+                            //retrieving info about what is user selling
+                            //userInfo.selling = val.items.selling;
+                            let selling = val.items.selling;
+                            for (let item in selling) {
+                                let key = JSON.parse('"'+htmlencode.htmlDecode(item)+'"');
+                                let toAdd = {};
+                                for (let prop in selling[item]) {
+                                    toAdd[prop] = JSON.parse('"'+htmlencode.htmlDecode(selling[item][prop])+'"')
+                                };
+                                toAdd.video_link = selling[item].video_link;
+                                userInfo.selling[key] = toAdd
                             }
                         })
                         .then(
-                            res.render('admin-dashboard', {title: 'Dashboard', status: 'in', admin: true, users: users})
+                            db.ref('/admin/users/'+userID).get()
+                            .then((data) => {
+                                let val = data.val();
+                                if (val.status){
+                                    userInfo.status = decrypt(val.status.encrypted, adminPWD.repeat(5).substring(0, 32), val.status.iv);
+                                } else {
+                                    //saving user's status to database, if none exists
+                                    db.ref('/admin/users/'+(userID).toString()+'/status').set(
+                                        encrypt('enabled', adminPWD.repeat(5).substring(0, 32))
+                                    )
+                                    userInfo.status = 'enabled';
+                                }
+                                userInfo.date = decrypt(val.date.encrypted, adminPWD.repeat(5).substring(0, 32), val.date.iv);
+                                userInfo.established = decrypt(val.established.encrypted, adminPWD.repeat(5).substring(0, 32), val.established.iv);
+                                userInfo.info.email = decrypt(val.email.encrypted, adminPWD.repeat(5).substring(0, 32), val.email.iv);
+                                userInfo.bought = decrypt(val.bought.encrypted, adminPWD.repeat(5).substring(0, 32), val.bought.iv);
+                                userInfo.sold = decrypt(val.sold.encrypted, adminPWD.repeat(5).substring(0, 32), val.sold.iv);
+                            })
+                            .then(() => {           
+                                console.log(userInfo);
+                                res.render('admin-user', {title: 'User', status: 'in', admin: true, userInfo: userInfo})
+                            })
                         )
                     } else {
                         res.redirect('/admin')
@@ -337,8 +307,7 @@ const getUser = async (req, res) => {
         })
 
     } else {
-        res.clearCookie('session');
-        res.render('login', {title: 'Login', name: '', data: req,  password_state: 'ok', scroll: 'false', user: 'none'})
+        res.redirect('/sessionLogout')
     }
 }
 
