1e87db9c70f0ddf8faf7aee750c65dcfb28691ff
norbertSviatko <informatikasviatko@gmail.com>
2022-04-03 12:37:06 +0200

    updated

---
+++ uploading.js
@@ -0,0 +1,8 @@
+
+// DEFINING THE QUEUE OF ITEMS WAITING TO BE UPLOADED AS A LIST
+const queue_to_upload = [];
+
+// DEFINING OBJECT OF CURRENTLY ADDING ITEMS
+const currently_adding = {}
+
+module.exports = { currently_adding, queue_to_upload }
